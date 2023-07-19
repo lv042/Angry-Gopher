332b363231f003bb2a0821df2f4574673fa4e335
Luca v. K <luca.v.kannen@gmail.com>
2023-07-19 13:58:13 +0200

    Updated ping, register and cmd route

--- src/server/main.go
+++ src/server/main.go
@@ -4,8 +4,8 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
 	log "github.com/sirupsen/logrus"
+	"math/rand"
 	"os"
-
 	"runtime"
 	"time"
 )
@@ -19,9 +19,14 @@ type SystemInfo struct {
 }
 
 type Device struct {
-	ID         int8       `json:"id"`
-	systemInfo SystemInfo // Updated: Made it public
-	lastOnline int64
+	ID          int8       `json:"id"`
+	SystemInfo  SystemInfo // Updated: Made it public
+	LastOnline  int64
+	CommandList CommandList
+}
+
+type CommandList struct {
+	Commands []string `json:"commands"`
 }
 
 func main() {
@@ -31,6 +36,30 @@ func main() {
 	token, _ := GenerateToken("admin")
 	log.Println("Token for admin: ", token)
 
+	devices = []Device{
+		{
+			ID: 1,
+			SystemInfo: SystemInfo{
+				Hostname:     "Device1",
+				OS:           "Linux",
+				Architecture: "x86_64",
+			},
+		},
+		{
+			ID: 2,
+			SystemInfo: SystemInfo{
+				Hostname:     "Device2",
+				OS:           "Windows",
+				Architecture: "amd64",
+			},
+		},
+	}
+
+	for i := range devices {
+		addRandomCommands(&devices[i].CommandList)
+	}
+
+	updateApplication(app)
 	logDevices()
 	setupRoutes(app)
 	serverListen(app)
@@ -42,8 +71,8 @@ func logDevices() {
 			for _, device := range devices {
 				log.WithFields(log.Fields{
 					"ID":         device.ID,
-					"SystemInfo": device.systemInfo,
-					"LastOnline": device.lastOnline,
+					"SystemInfo": device.SystemInfo,
+					"LastOnline": device.LastOnline,
 				}).Info("Logging device -> ")
 			}
 
@@ -56,6 +85,7 @@ func logDevices() {
 		}
 	}()
 }
+
 func setupDotenv() {
 	err := godotenv.Load("../.env")
 	if err != nil {
@@ -66,5 +96,44 @@ func setupDotenv() {
 	log.Println("Environment variables:")
 	for _, e := range os.Environ() {
 		log.Println(e)
+	}
+}
+
+func updateApplication(app *fiber.App) {
+	updateLastOnline()
+
+	go func() {
+		for {
+			//TODO: update this to use a database
+		}
+	}()
+}
+
+func updateLastOnline() {
+	go func() {
+		for {
+			for i := range devices {
+				// Increment the LastOnline count by 1
+				devices[i].LastOnline++
+			}
+
+			time.Sleep(1 * time.Second)
+		}
+	}()
+}
+
+func addRandomCommands(commandList *CommandList) {
+	commands := []string{"command1", "command2", "command3"}
+
+	// Generate a random number of commands to add (between 1 and 3)
+	numCommands := rand.Intn(3) + 1
+
+	for i := 0; i < numCommands; i++ {
+		// Choose a random command from the list
+		randomIndex := rand.Intn(len(commands))
+		randomCommand := commands[randomIndex]
+
+		// Add the random command to the command list
+		commandList.Commands = append(commandList.Commands, randomCommand)
 	}
 }
--- src/server/router.go
+++ src/server/router.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/gofiber/fiber/v2"
 	"log"
 	"strconv"
@@ -21,22 +22,44 @@ func setupRoutes(app *fiber.App) {
 		id := len(devices) + 1
 		device := Device{
 			ID:         int8(id),
-			systemInfo: systemInfo,
-			lastOnline: 0,
+			SystemInfo: systemInfo,
+			LastOnline: 0,
 		}
 		devices = append(devices, device)
 
 		//return their id
 		return c.JSON(fiber.Map{"id": id})
 	})
-	protected.Get("/ping:id", func(c *fiber.Ctx) error {
-		var id, err = strconv.Atoi(c.Params("id"))
-		if err != nil {
+
+	protected.Get("/ping/:id", func(c *fiber.Ctx) error {
+		id, err := strconv.Atoi(c.Params("id"))
+		if err != nil || id < 1 || id > len(devices) {
 			return fiber.ErrBadRequest
 		}
-		devices[int(id)-1].lastOnline = 0
 
-		return c.SendString("pong")
+		device := devices[id-1]
+		device.LastOnline = 0
+		devices[id-1] = device
+
+		return c.JSON(fiber.Map{"message": "Pong!"})
+	})
+	protected.Post("/cmd/:id", func(c *fiber.Ctx) error {
+		id, err := strconv.Atoi(c.Params("id"))
+		if err != nil || id < 1 || id > len(devices) {
+			return fiber.ErrBadRequest
+		}
+
+		// Get the command from the query parameter
+		command := c.Query("command")
+
+		// Generate a response command for the client
+		response := struct {
+			Command string `json:"command"`
+		}{
+			Command: fmt.Sprintf("Received command '%s'", command),
+		}
+
+		return c.JSON(response)
 	})
 
 }
